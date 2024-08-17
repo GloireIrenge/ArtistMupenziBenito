e12b9841861726cceb10b996d9fc84ba42fa17fa
Gloire <mufungizigloire1@gmail.com>
2024-08-17 06:07:22 +0200

    buiding some basic files

---
+++ artistmupenzibenito/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about">
+      <h2>About</h2>
+      <p>Learn more about our website and what we do.</p>
+    </section>
+  );
+}
+
+export default About;
